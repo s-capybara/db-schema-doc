721c80b16b25d500eb2b2944be7bb623f41454bc
Shohei Kajihara <s.white.capybara@gmail.com>
2018-03-21 18:47:39 +0900

    Add basic implementation
    WIP: Add simple implementation
    
    WIP: Move main code to lib
    
    WIP: extract methods
    
    Add basic implementation

--- cmd/root.go
+++ cmd/root.go
@@ -7,6 +7,10 @@ import (
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/s-capybara/db-schema-markdown/lib"
 )
 
 var cfgFile string
@@ -17,10 +21,38 @@ var rootCmd = &cobra.Command{
 	Short: "Generates database definition for Markdown from existing table",
 	Long: `"db-schema-markdown" is a CLI tool to generate a database definition document for Markdown
 from an existing database table.
-Useful when modifying schema.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+
+Positional arguments specify columns to show.`,
+	Run: Root,
+}
+
+func Root(cmd *cobra.Command, args []string) {
+	dbsm.Root(Auth(), Table(), Columns(cmd, args))
+}
+
+func Auth() dbsm.Auth {
+	return dbsm.Auth{
+		Username: viper.GetString("username"),
+		Password: viper.GetString("password"),
+		Database: viper.GetString("database")}
+}
+
+func Table() string {
+	return viper.GetString("table")
+}
+
+func Columns(cmd *cobra.Command, args []string) []string {
+	// TODO: It's better to get columns by flags.
+	if viper.GetBool("full") {
+		// Full columns.
+		return []string{"Field", "Type", "Collation", "Null", "Key", "Default", "Extra", "Privileges", "Comment"}
+	} else if len(args) == 0 {
+		// Standard columns.
+		return []string{"Field", "Type", "Null", "Default", "Comment"}
+	} else {
+		// Specified columns.
+		return args
+	}
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -35,14 +67,22 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.db-schema-markdown.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default $HOME/.db-schema-markdown.yml)")
+	rootCmd.PersistentFlags().BoolP("full", "f", false, "shows all columns if true")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringP("username", "u", "root", "username for database")
+	rootCmd.Flags().StringP("password", "p", "", "password for database")
+	rootCmd.Flags().StringP("database", "D", "", "database name")
+	rootCmd.Flags().StringP("table", "t", "", "table name")
+
+	rootCmd.MarkFlagRequired("database")
+	rootCmd.MarkFlagRequired("table")
+
+	viper.BindPFlag("username", rootCmd.Flags().Lookup("username"))
+	viper.BindPFlag("password", rootCmd.Flags().Lookup("password"))
+	viper.BindPFlag("database", rootCmd.Flags().Lookup("database"))
+	viper.BindPFlag("table", rootCmd.Flags().Lookup("table"))
+	viper.BindPFlag("full", rootCmd.PersistentFlags().Lookup("full"))
 }
 
 // initConfig reads in config file and ENV variables if set.
